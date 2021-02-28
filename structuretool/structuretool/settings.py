dd7dad9434dcea8177c3963395c9778ec0594ca1
Sandipan Ganguly <sandipanganguly96@gmail.com>
2021-02-28 20:55:22 +0530

    Created User App. Added Registration Page

--- structuretool/structuretool/settings.py
+++ structuretool/structuretool/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'structure.apps.StructureConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

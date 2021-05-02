5b501db05711885d15bb5e061bd5ebeb3e0f95b9
Sandipan Ganguly <sandipanganguly96@gmail.com>
2021-05-02 21:51:08 +0530

    adding initial report styling

--- structuretool/structure/views.py
+++ structuretool/structure/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import (
     ListView, 
@@ -9,6 +10,7 @@ from django.views.generic import (
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .forms import ProjectDetailsForm, MatStrengthForm, SectionLibraryForm, SectionFormTwo
@@ -16,6 +18,7 @@ from .models import ProjectDetails, SimpleTable, AlloyGrade, MatStrength, Sectio
 from extra_views import CreateWithInlinesView, InlineFormSetFactory
 from xhtml2pdf import pisa
 from django.template.loader import get_template
+from django.contrib.staticfiles import finders
 
 
 def home(request):
@@ -286,7 +289,36 @@ def sectionView(request,pk):
                 'mdCoef':mdCoef, 'length':length, 'lwidth':lwidth, 'rwidth':rwidth }
     return render(request,'structure/sectionView.html',context)
 
+# def link_callback(uri, rel):
+#             """
+#             Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+#             resources
+#             """
+#             result = finders.find(uri)
+#             if result:
+#                     if not isinstance(result, (list, tuple)):
+#                             result = [result]
+#                     result = list(os.path.realpath(path) for path in result)
+#                     path=result[0]
+#             else:
+#                     sUrl = settings.STATIC_URL        # Typically /static/
+#                     sRoot = settings.STATIC_ROOT      # Typically /home/userX/project_static/
+#                     mUrl = settings.MEDIA_URL         # Typically /media/
+#                     mRoot = settings.MEDIA_ROOT       # Typically /home/userX/project_static/media/
 
+#                     if uri.startswith(mUrl):
+#                             path = os.path.join(mRoot, uri.replace(mUrl, ""))
+#                     elif uri.startswith(sUrl):
+#                             path = os.path.join(sRoot, uri.replace(sUrl, ""))
+#                     else:
+#                             return uri
+
+#             # make sure that file exists
+#             if not os.path.isfile(path):
+#                     raise Exception(
+#                             'media URI must start with %s or %s' % (sUrl, mUrl)
+#                     )
+#             return path
 
 def windowsPDF(request,pk):
     template_path = "structure/windowsPDF.html"
